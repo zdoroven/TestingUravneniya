b7973ea8b927b862437b5751fdf084f5a1e6ee02
zdoroven <45197696+zdoroven@users.noreply.github.com>
2019-05-30 17:17:56 +0300

    Update myfunctest.h

--- tests/myfunctest.h
+++ tests/myfunctest.h
@@ -4,34 +4,12 @@
 #include <gtest/gtest.h>
 
 extern "C" {
-#include "myfunc.h"
+#include "functions.h"
 }
 
-TEST(myfuncTest, num0) {
-    ASSERT_EQ(myfunc(3, 2, 1).flag, 0);
-    ASSERT_EQ(myfunc(1, 1, 1).flag, 0);
+TEST(calculate, num0){
+    ASSERT_EQ(calculate(0, 5), 5);
+    ASSERT_EQ(calculate(5, 0), 5);
+    ASSERT_EQ(calculate(5, 5), 10);
 }
-
-TEST(myfuncTest, num1) {
-    ASSERT_EQ(myfunc(0, 5, 5).flag, 1);
-
-}
-
-TEST(myfuncTest, num2) {
-    ASSERT_EQ(myfunc(-1, -5, -3).flag, 2);
-}
-
-TEST(myfuncTest, num3) {
-    ASSERT_EQ(myfunc(0, 0, 0).flag, 3);
-}
-
-TEST(myfuncTest, num4) {
-    ASSERT_EQ(myfunc(1, -5, 6).x1, 2);
-    ASSERT_EQ(myfunc(1, -5, 6).x2, 3);
-}
-
-TEST(myfuncTest, num5) {
-    ASSERT_EQ(myfunc(2, 4, 2).flag, 1);
-}
-
 #endif // MYFUNCTEST_H
