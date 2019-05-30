3ceb6912c3f43a0e69db4a0bccedc48dd6a8ada2
zdoroven <45197696+zdoroven@users.noreply.github.com>
2019-05-30 17:19:39 +0300

    Update myfunc.c

--- app/myfunc.c
+++ app/myfunc.c
@@ -1,29 +1,7 @@
 #include "myfunc.h"
 #include <math.h>
 
-name myfunc(double a, double b, double c) {
-    name roots;
-    double d = b * b - 4 * a * c;
-    if (d < 0.0){
-        roots.flag = 0;
-        return roots;
-    }
-    if (d == 0.0) {
-        roots.flag = 1;
-    }
-    roots.x1 = (b * (-1) - sqrt(d))/ (2 * a);
-
-    roots.x2 = (b * (-1) + sqrt(d))/ (2 * a);
-    if (a == 0.0 && b == 0.0 && c == 0.0){
-        roots.flag = 3;
-    } else if (d > 0.0){
-        roots.flag = 2;
-    }
-    if (a == 0.0 ){
-        if(b != 0.0 || c != 0.0){
-            roots.flag = 1;
-            roots.x1 = c * (-1) / b;
-        }
-    }
-    return roots;
+int calculate(int sum, int k){
+    sum += k;
+    return sum;
 }
