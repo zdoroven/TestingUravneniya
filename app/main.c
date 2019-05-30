78e94fa61be28b6b3260ebe6cb76685c4c599662
zdoroven <45197696+zdoroven@users.noreply.github.com>
2019-05-30 17:43:30 +0300

    Update main.c

--- app/main.c
+++ app/main.c
@@ -3,21 +3,6 @@
 
 int main()
 {
-    double a = 0.0, b = 0.0, c = 0.0;
-    name otvet;
-
-    otvet = myfunc(a, b, c);
-    if (otvet.flag == 0){
-        printf("Корней нет\n");
-    }
-    if (otvet.flag == 1){
-        printf("Один корень: %lf\n", otvet.x1);
-    }
-    if (otvet.flag == 2){
-        printf("Первый корень: %lf\nВторой корень: %lf\n", otvet.x1, otvet.x2);
-    }
-    if (otvet.flag == 3) {
-        printf("Корней бесконечно много\n");
-    }
+    printf("hello world");
     return 0;
 }
