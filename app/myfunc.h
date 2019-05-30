5b9556eb7c20b6e1c1817142276c64b1625adbd2
zdoroven <45197696+zdoroven@users.noreply.github.com>
2019-05-30 17:20:05 +0300

    Update myfunc.h

--- app/myfunc.h
+++ app/myfunc.h
@@ -1,12 +1,6 @@
 #ifndef MYFUNC_H
 #define MYFUNC_H
 
-typedef struct _name
-{
- double x1;
- double x2;
- int flag;
-} name;
-name myfunc(double a, double b, double c);
+int calculate(int sum, int k);
 
 #endif // MYFUNC_H
